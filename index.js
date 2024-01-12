7b9ce91d91f1173a97b1c81ce256efab214cb720
AntonioEspin <antoesdev91@gmail.com>
2024-01-12 12:16:42 -0500

    ADD styles prices component

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const slider = document.querySelector('.slider')
+const slider = document.querySelector('.slider');
+const numberViews = document.querySelector('.pageviews span');
 
 slider.addEventListener('input', getValueInput)
 slider.addEventListener('mousemove', getValueMouse)
 
 function getValueInput () {
-  console.log('input',this.value)
   let value = this.value
+
+  // Cambiar dinamicamente color del slider
   let color = `linear-gradient(90deg, var(--Strong-Cyan) ${value}%, var(--Light-Grayish-Blue) ${value}%)`
   slider.style.background = color
+
+  // cambiar valor del texto
+  numberViews.textContent = value
 }
+
 function getValueMouse () {
   console.log('MouseMove',slider.value)
 }
