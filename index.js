f47c5d5a3cd1b8f7cb933d2a0dff24bd03823b97
AntonioEspin <antoesdev91@gmail.com>
2024-01-17 06:19:25 -0500

    fix bugs

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const slider = document.querySelector('.slider');
 const numberViews = document.querySelector('.pageviews span');
+const amount = document.querySelector('.amount strong');
 
-slider.addEventListener('input', getValueInput)
-slider.addEventListener('mousemove', getValueMouse)
+const circle = document.querySelector('.toggle-button div');
+const button = document.querySelector('.toggle-button');
+button.addEventListener('click', toggleButton);
 
-function getValueInput () {
-  let value = this.value
+slider.addEventListener('input', updatePrice);
 
-  // Cambiar dinamicamente color del slider
-  let color = `linear-gradient(90deg, var(--Strong-Cyan) ${value}%, var(--Light-Grayish-Blue) ${value}%)`
-  slider.style.background = color
+function updatePrice() {
+  const value = slider.value;
 
-  // cambiar valor del texto
-  numberViews.textContent = value
+  // Cambiar dinámicamente el color del slider
+  const color = `linear-gradient(90deg, var(--Strong-Cyan) ${value}%, var(--Light-Grayish-Blue) ${value}%)`;
+  slider.style.background = color;
+
+  // Cambiar el valor del texto y monto
+  const pricing = calculatePricing(value);
+  numberViews.textContent = pricing.pageviews;
+  amount.textContent = `$${pricing.amount}.00`;
 }
 
-function getValueMouse () {
-  console.log('MouseMove',slider.value)
-}
+function calculatePricing (value) {
+  let pageviews, amount;
+
+  switch (value) {
+    case '0':
+      pageviews = '10k';
+      amount = 8;
+      break;
+    case '25':
+      pageviews = '50k';
+      amount = 12;
+      break;
+    case '75':
+      pageviews = '500k';
+      amount = 24;
+      break;
+    case '100':
+      pageviews = '1M';
+      amount = 36;
+      break;
+    default:
+      pageviews = '100K';
+      amount = 16;
+      break;
+  }
+
+  // Aplicar descuento si está activo
+  if (circle.classList.contains('active-toggle')) {
+    const discount = amount * 0.25;
+    amount -= discount;
+  }
+
+  return { pageviews, amount };
+};
+
+function toggleButton () {
+  // Cambiar estilos
+  button.classList.toggle('background-toggle');
+  circle.classList.toggle('active-toggle');
+
+  // cambiar el valor dependiendo el descuento
+  updatePrice()
+};
